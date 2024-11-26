01cc2a50e70b9aebe97385d6f41f430bf59a60b3
Wutipong Wongsakuldej <mr_tawan@hotmail.com>
2024-11-26 08:05:30 +0700

    Initial commit.

---
+++ GalleryBackend/Program.cs
@@ -0,0 +1,53 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
+
+var summaries = new[]
+{
+    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+};
+
+app.MapGet("/weatherforecast", () =>
+{
+    var forecast = Enumerable.Range(1, 5).Select(index =>
+        new WeatherForecast
+        (
+            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+            Random.Shared.Next(-20, 55),
+            summaries[Random.Shared.Next(summaries.Length)]
+        ))
+        .ToArray();
+    return forecast;
+})
+.WithName("GetWeatherForecast");
+
+const string BASE_DIRECTORY = "/data";
+app.MapGet("/list", (String path="") =>
+{
+    var actualPath = Path.Combine(BASE_DIRECTORY, path);
+    var directories = Directory.GetDirectories(actualPath);
+    var files = Directory.GetFiles(actualPath);
+
+    var output = new ListResult(directories, files);
+
+    return output;
+}).WithName("List");
+
+app.Run();
+
+internal record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+{
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+}
+
+internal record ListResult(String[] Directories, String[] files) { }
